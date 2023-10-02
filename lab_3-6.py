9110cdbb89784bab087eb8b8954eba3b78273755
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-10-02 14:18:27 -0400

    finished

---
+++ lab_3-6.py
@@ -0,0 +1,14 @@
+
+#Create a Python file named lab_3-6.py
+
+#Write a statement that creates the string "Fairfield Prep" by concatenating substrings.
+print("fairfield" + "prep")
+#Write a statement that creates a dashed line that is 20 dashes long.
+print ("-" + "20")
+#Write a statement that prints the following:
+#I'm loving this short story I'm reading, "The Fall of the House of Usher"
+print ("i\'m loving this short story i\'m reading\", \"the fall of the house of usher") 
+#Write a statement that prints the length of the previous string.
+print(len("i\'m loving this short story i\'m reading\",\"the fall of the house of usher"))
+#Write a statement using the index operator that returns the "L" in "apple"
+print("apple"[3])
